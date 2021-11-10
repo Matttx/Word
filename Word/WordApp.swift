e4cbbc7acfa38596c291099b3911f4c8db06692f
mattéo <fauchonmatteo@gmail.com>
2021-11-10 19:01:47 +0100

    Initial Commit

---
+++ Word/WordApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordApp.swift
+//  Word
+//
+//  Created by Matteo on 10/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
